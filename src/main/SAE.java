c9c79108af486f3cb948a5423920f5f2252236e6
AG <iusehaxbro@gmail.com>
2024-11-18 20:36:48 +0100

    Javadoc finito

--- src/main/SAE.java
+++ src/main/SAE.java
@@ -5,27 +5,28 @@
 package main;
 
 import map.MapViewerPanel;
+import org.graphstream.graph.Graph;
+import sae.Graphegen;
 
 public class SAE {
 
 
     public static void main(String[] args) {
-        String fichier = "Data_Test/vol-test4-debug.csv";
-//
-//        Graphegen g = new Graphegen("aeroports.txt", fichier);
-//        g.setOutput(true);
-//        Graph graph = g.genGraph();
-//        graph.display();
+        String fichier = "Data_Test/vol-test4.csv";
 
-        MapViewerPanel mapViewerPanel = new MapViewerPanel();
-        mapViewerPanel.visualizeVierge();
+        Graphegen g = new Graphegen("aeroports.txt", fichier);
+        g.setOutput(true);
+        Graph graph = g.genGraph();
+        graph.display();
 
-        mapViewerPanel.close();
+        MapViewerPanel mapViewerPanel = new MapViewerPanel(fichier);
+        mapViewerPanel.visualize();
+
 
 
 
 //        for (int i = 0; i <= 9; i++) {
-//            Graphegen gg = new Graphegen("aeroports.txt", "Data_Test/vol-test"+i+".csv");
+//            Graphegen gg = new Graphegen("aeroports.txt", "vol-evalP1/vol-eval"+i+".csv");
 //            Graph graphe = gg.genGraph();
 //        }
 
